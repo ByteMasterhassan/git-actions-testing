a97ef39fb468a5d89bd02d3b314b7b4588af1baf
bytemasterhassan <hassanshahid340@gmail.com>
2023-11-20 09:57:48 +0500

    adding the file for tetsing

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/abc')
+def index():
+    return render_template('attractive_face.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
